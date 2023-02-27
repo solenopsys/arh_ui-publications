c6d8d90a4d2d24421b8d876f2a959153718329e7
AlexStorm <info@alexstorm.ru>
2023-02-27 06:49:50 +0530

    reintegrate

--- src/lib/ui-public.module.ts
+++ src/lib/ui-public.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextPageGroupComponent } from "./text-page-group/text-page-group.component";
-import { UIEditorModule } from "@solenopsys/ui-editor-content";
+
 import {DeclaredService} from "@solenopsys/ui-utils";
 
 const components = [TextPageGroupComponent];
 
 @NgModule({
   declarations: components,
-  imports: [CommonModule, UIEditorModule]
+  imports: [CommonModule]
 })
 export class UIPublicModule {
   constructor(private ds: DeclaredService) {
